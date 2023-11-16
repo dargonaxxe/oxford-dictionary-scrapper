1ecfb0dd5d1312c43d4e644dc98a01acbdf58424
Ilia Kuzmin <dargonaxxe@gmail.com>
2023-11-16 17:28:59 +0400

    CLI application

--- lib/scrapper.ex
+++ lib/scrapper.ex
@@ -3,6 +3,46 @@ defmodule Scrapper do
   Main module that basically connects all the other modules together.
   """
 
+  defmodule CLI do
+    @moduledoc """
+    Module that contains all the methods related to work with CLI
+    """
+    def main(args) do
+      params =
+        optimus_config()
+        |> Optimus.parse!(args)
+
+      Scrapper.scrap(params.args.words_file, params.args.output_file)
+    end
+
+    defp optimus_config() do
+      Optimus.new!(
+        name: "oxscrapper",
+        description: "Oxford dictionary scrapper.",
+        version: "0.0.1",
+        author: "Ilia Kuzmin dargonaxxe@gmail.com",
+        about:
+          "Utility that scraps necessary data for specified words from Oxford dictionary website.",
+        allow_unknown_args: false,
+        parse_double_dash: false,
+        args: [
+          words_file: [
+            value_name: "WORDS_FILE",
+            help: "File with words of interest separated by semicolon",
+            required: true,
+            parser: :string
+          ],
+          output_file: [
+            value_name: "OUTPUT_FILE",
+            help: "Path to output .csv file",
+            required: true,
+            parser: :string
+          ]
+        ]
+      )
+    end
+  end
+
   @spider Oxford.Spider
   def scrap(words_file, output_file) do
     urls = Oxford.LinkBuilder.from_file!(words_file)
